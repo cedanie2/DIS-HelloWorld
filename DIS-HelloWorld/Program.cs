78108c1748fdf8c3dc4b78473fdc2a2ddb910c90
Clinton Daniel <cedanie2@usf.edu>
2019-08-27 19:46:53 -0400

    Add project files.

---
+++ DIS-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIS_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
